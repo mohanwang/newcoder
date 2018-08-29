acd61cafd68feb1f7d6b464e3134afe7ba470e93
mohanwang <1057783744@qq.com>
2018-08-29 18:51:14 +0800

    增加随机数字输出

--- newcoder/newcoder/main.cpp
+++ newcoder/newcoder/main.cpp
@@ -9,10 +9,32 @@
 #include <iostream>
 #include "Solution.hpp"
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     Solution solution;
     std::cout << solution.NumberOf1(-1) << std::endl;
-    std::cout << "Hello, World!\n";
+    
+    srand(time(NULL));
+    int sum = 0;
+    cout << "----small---" << endl;
+    for (int i = 0; i < 25; i++) {
+        int small_money = (rand() % 40 + 10) * 10 + rand() % 10;
+        cout << small_money << endl;
+        sum += small_money;
+    }
+    cout << "---middle----" << endl;
+    for (int i = 0; i < 3; i++) {
+        int middle_money = (rand() % 3 + 1) * 1000 + rand() % 10 * 100 + rand() % 10 * 10 + rand() % 10;
+        cout << middle_money << endl;
+        sum += middle_money;
+    }
+    cout << "---big----" << endl;
+    int big_money = rand() % 10 * 100 + rand() % 10 * 10 + rand() % 10 + 5000;
+    cout << big_money << endl;
+    sum += big_money;
+    cout << "---count----" << endl;
+    cout << sum << endl;
     return 0;
 }
